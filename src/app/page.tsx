ec7eccf8c2bbb8bb971f9bfa653a75a5f457e64e
Zakir-Hussain313 <regalt0s375@gmail.com>
2026-01-24 17:18:38 +0500

    right click disabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import { Hero } from './components/Hero';
-import { ImpactStats } from './components/ImpactStats';
-import { FeaturedProjects } from './components/FeaturedProjects';
-import { Testimonials } from './components/Testimonials';
-import FAQSection from './components/FAQ';
+import DisableRightClick from "./components/DisableRightClick";
+import { Hero } from "./components/Hero";
+import { ImpactStats } from "./components/ImpactStats";
+import { FeaturedProjects } from "./components/FeaturedProjects";
+import { Testimonials } from "./components/Testimonials";
+import FAQSection from "./components/FAQ";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-white">
+      <DisableRightClick />
       <Hero />
       <ImpactStats />
       <FeaturedProjects />
